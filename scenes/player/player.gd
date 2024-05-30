2181cd5a179df3a8c567ce2ad2e876ec0b0df237
Vinicios <vinicioscsoares@gmail.com>
2024-05-30 14:21:12 -0300

    colission test

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
 
-func _process(delta):
+func _process(_delta):
 	var direction = Input.get_vector("left","right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
 
 	#laser shooting input
 	
